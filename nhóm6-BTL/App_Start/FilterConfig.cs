85a375a370ca4832e7d880d3ef65a3ec20617fba
NGUYEN THI LAN ANH <1721050613@student.humg.edu.vn>
2021-10-08 13:12:57 +0700

    Add project files.

---
+++ nhóm6-BTL/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace nhóm6_BTL
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ nhóm6-BTL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nhóm6_BTL.MvcApplication" Language="C#" %>
