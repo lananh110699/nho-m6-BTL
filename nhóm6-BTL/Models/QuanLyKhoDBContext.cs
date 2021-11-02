e2bdbbffe289365161dc709b97ec0a6bccd6ded8
NGUYEN THI LAN ANH <1721050613@student.humg.edu.vn>
2021-11-02 13:03:04 +0700

    upp

---
+++ nhóm6-BTL/Models/QuanLyKhoDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace nhóm6_BTL.Models
+{
+    public partial class QuanLyKhoDBContext : DbContext
+    {
+        public QuanLyKhoDBContext()
+            : base("name=QuanLyKhoDBContext")
+        {
+        }
+
+        public virtual DbSet<DangNhap> DangNhaps { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DangNhap>()
+                .Property(e => e.UserName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<DangNhap>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ nhóm6-BTL/Models/XuatKho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nhóm6_BTL.Models
+{
+    public class XuatKho
+    {
+        public int MaHang { get; set; }
+        public int TenHang { get; set; }
+        public int SoLuong { get; set; }
+        public int DonGia { get; set; }
+    }
+}
